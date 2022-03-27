0c7ee60968ece6832b80e6006134344bc71d85aa
Christ Kamanda <kamandachristo@gmail.com>
2022-03-27 15:54:19 -0400

    prework js functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+
+function saturdayFun(){
+    console.log(`This Saturday, I want to ...!`);
+}
+function saturdayFun(overriden = `bathe my dog!`, activity = `roller-skate!`){
+    console.log(`${overriden} bathe my dog!`)(`${activity} roller-skate!`)
+}
+
+
+
+function mondayWork() {
+  return `This monday, I will work from home.`
+}
+
+
+
+function wrapAdjective(string){
+    return function(){
+        return `You are *a hard worker`
+    }
+
+}
